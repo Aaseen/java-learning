2d66163d6ded585e4c16221263d78d11f873ded0
Aaseen <aaseenbodapati@gmail.com>
2026-08-20 17:29:21 +0530

    Today Learning is "JAVA DATATYPES."
    learning how to be consistent with my work....

---
+++ DataTypes.java
@@ -0,0 +1,22 @@
+class DataTypes
+{
+	public static void main(String args[])
+	{
+	int v = 3;
+	char x = 'a';
+	float y = (float)6.77;
+	double z = 6.78;
+	System.out.println("THE INT VALUES IS"+v);
+	System.out.println("THE CHAR VALUES IS"+x);
+	System.out.println("THE FLOAT VALUES IS"+y);
+	System.out.println("THE DOUBLE VALUES IS"+z);
+	System.out.printf("THE ground VALUES IS %g\n",y);
+	System.out.printf("THE exponential VALUES IS %e\n",y);
+	System.out.printf("THE CHAR VALUES IS %c\n",x);
+	System.out.printf("THE CHAR VALUES IS %C\n",x);
+	boolean f = true;
+	System.out.printf("THE boolean VALUES IS %b\n",x);
+	System.out.printf("THE boolean VALUES IS %B\n",x);
+	System.out.println("THE boolean VALUES IS"+f);
+	}
+}
