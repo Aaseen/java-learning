30750d2faac85fc12cf08e24a6e10048c7b2baa4
Aaseen <aaseenbodapati@gmail.com>
2026-08-20 17:42:24 +0530

    Add basic arithmetic operations in operators.java

---
+++ operators.java
@@ -0,0 +1,15 @@
+class operator
+  {
+    public Static void main(String args[])
+    {
+      int x = 9;
+      int y = 10;
+      System.out.println("THE ADDITION IS"+(x+y));
+      System.out.println("THE SUBTRACTION IS"+(x-y));
+      System.out.println("THE MULTIPLICATION IS"+(x*y));
+      System.out.println("THE DIVISION IS"+(x/y));
+      System.out.println("THE REMINDER IS"+(x%y));
+      System.out.println("THE PREINCREMENT OF X IS"+(++x));
+      System.out.println("THE POSTINCREMENT OF X IS"+(x++));
+    }
+  }
